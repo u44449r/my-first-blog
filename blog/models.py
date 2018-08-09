95ea31fbfbd0ac305248835fa5af1d51d413da5a
u44449r <u44449r@gmail.com>
2018-08-09 12:59:59 +0900

    set template

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
+class Post(models.Model):#ブログ記事のデータモデル
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
