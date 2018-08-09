a05ca0d9361a3fe68be1cb70891207ad829555c2
u44449r <u44449r@gmail.com>
2018-08-09 16:02:57 +0900

    formを設定

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post #モデルをインポート。「.」は現在のデ�
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404 #404ページ用
 from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -13,6 +14,16 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
